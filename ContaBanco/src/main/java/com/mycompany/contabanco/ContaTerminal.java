f5f8cfad8b7d295c89ed1218e1aa428359c6edf7
Lucas de Souza Almeida <Lucas.Almeida@SCCLGS.INTRA>
2023-10-25 12:04:34 -0300

    Recebe dados e retorna mensagem formatada da conta criada.

--- ContaBanco/src/main/java/com/mycompany/contabanco/ContaTerminal.java
+++ ContaBanco/src/main/java/com/mycompany/contabanco/ContaTerminal.java
@@ -4,13 +4,28 @@
 
 package com.mycompany.contabanco;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Lucas.Almeida
  */
 public class ContaTerminal {
-
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        /*Fazer utilização da classe Scanner
+        * Exibir as mensagens, receber falores e exibir texto formatado com a conta.
+        */
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Informe o nome do cliente: ");
+        String nome = scanner.next();
+        System.out.print("Informe a agencia: ");
+        int agencia = scanner.nextInt();
+        System.out.print("Informe numero da conta: ");
+        int numeroConta = scanner.nextInt();
+        System.out.print("Informe o saldo: ");
+        float saldoConta = scanner.nextFloat();
+        
+        System.out.println("Ola " + nome + ", obrigado por criar uma conta em nosso banco, sua agencia eh: " + agencia + ", conta" + numeroConta + " e seu saldo " + saldoConta + " ja esta disponivel para saque.");
     }
 }
