ae7f0a6e314c82d24991618fb0bbca52cc92322b
Lucas de Souza Almeida <Lucas.Almeida@SCCLGS.INTRA>
2023-10-25 09:58:39 -0300

    primeiro commit

---
+++ ContaBanco/src/main/java/com/mycompany/contabanco/ContaTerminal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.contabanco;
+
+/**
+ *
+ * @author Lucas.Almeida
+ */
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
